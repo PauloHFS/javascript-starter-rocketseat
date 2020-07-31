d792e70fe898ddea0cee4d7687cd87b53d1a3426
Paulo Hernane <paulohernane10@gmail.com>
2020-07-31 01:18:12 -0300

    Módulo 3/

--- Módulo 3/todo.js
+++ Módulo 3/todo.js
@@ -2,19 +2,23 @@ var ulElement = document.querySelector("#app ul");
 var inputElement = document.querySelector("#app input");
 var btnElement = document.querySelector("#app button");
 
-var todos = [
-    'Fazer café',
-    'Estudar Javascript',
-    'Acessar comunidade da Rocketseat'
-];
+var todos = JSON.parse(localStorage.getItem('list_todos')) || [];
 
 function renderTodos() {
     ulElement.innerHTML = "";
     for (todo of todos) {
         var todoElement = document.createElement("li");
         var todoText = document.createTextNode(todo);
-
         todoElement.appendChild(todoText);
+
+        var linkElement = document.createElement('a');
+        linkElement.setAttribute("href", "#");
+        var pos = todos.indexOf(todo);
+        var linkText = document.createTextNode("Excluir");
+        linkElement.setAttribute('onclick', 'deleteTodo('+ pos +')');
+        linkElement.appendChild(linkText);
+        todoElement.appendChild(linkElement);
+
         ulElement.appendChild(todoElement);
     }
 }
@@ -28,4 +32,15 @@ btnElement.onclick = function () {
     todos.push(todoText);
     
     renderTodos();
+    saveToStorage();
+}
+
+function deleteTodo(pos) {
+    todos.splice(pos, 1);
+    renderTodos();
+    saveToStorage();
+}
+
+function saveToStorage() {
+    localStorage.setItem("list_todos", JSON.stringify(todos));
 }
